4590124c11e9a0974949c8d51f3f0f06424f5157
Anton Mosin <aamosin@edu.hse.ru>
2024-03-11 15:54:41 +0500

    added comb complex in pybind

--- module_creation.cpp
+++ module_creation.cpp
@@ -1,21 +1,30 @@
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 
+#include "src/CombinatorialComplex.h"
 #include "src/HyperGraph.h"
 #include "src/SimplicialComplex.h"
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(simpl, m) {
-    m.doc() = "simplex and etc";
+    m.doc() = "SimplicialComplex, HyperGraph & CombinatorialComplex";
 
     py::class_<SimplicialComplex>(m, "SimplicialComplex")
         .def(py::init<>())
         .def("AddComplex", &SimplicialComplex::AddComplex)
-        .def("RemoveComplex", &SimplicialComplex::RemoveComplex);
+        .def("RemoveComplex", &SimplicialComplex::RemoveComplex)
+        .def("Incidence", &SimplicialComplex::Incidence)
+        .def("Degree", &SimplicialComplex::Degree);
 
     py::class_<HyperGraph>(m, "HyperGraph")
         .def(py::init<>())
         .def("AddEdge", &HyperGraph::AddEdge)
         .def("RemoveEdge", &HyperGraph::RemoveEdge);
+
+    py::class_<CombinatorialComplex>(m, "CombinatorialComplex")
+        .def(py::init<>())
+        .def("Build", &CombinatorialComplex::Build)
+        .def("Incidence", &CombinatorialComplex::Incidence)
+        .def("Degree", &CombinatorialComplex::Degree);
 }
